08c00abae2f5ad29dc1722ea437dc5e9a1b10350
liuzhiqi <j419230944@hotmail.com>
2022-07-25 15:48:17 +0800

    add  不同的二叉搜索树.md

--- PythonLearning/main.py
+++ PythonLearning/main.py
@@ -3,14 +3,21 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
+from sortedcontainers import SortedList
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def test():
 
+    a = SortedList()
+    a.add([16, "sushi"])
+    a.add([16, "ramen"])
+    a.add([15, "gg"])
+    print(a)
+
+def NumberOf1(n):
+    return bin(n & 0xffffffff).count('1')
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print(NumberOf1(5))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
