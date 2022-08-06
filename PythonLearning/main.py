2300682de39abd3872a75ac8a08be5ed0605aaf1
liuzhiqi <j419230944@hotmail.com>
2022-08-06 12:41:10 +0800

    update

--- PythonLearning/main.py
+++ PythonLearning/main.py
@@ -2,22 +2,52 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+def solution(S):
+    # write your code in Python (Python 3.6)
+    ans = float('inf')
+    # if len(S) > 100000:
+    #     return -1
 
-from sortedcontainers import SortedList
+    ind = []
+    distance = []
+    for i in range(len(S)):
+        if S[i] == 'R':
+            if ind:
+                distance.append(i - ind[-1] - 1)
+            ind.append(i)
+    if not ind:
+        return 0
+    sumValue = sum(distance)
+    if sumValue >= 10 ** 9:
+        return -1
+    front = [0]
+    backend = [sumValue]
+    for i in distance:
+        front.append(i + front[-1])
+        backend.append(backend[-1] - i)
+    for i in range(len(front)):
+        ans = min(front[i] + backend[i], ans)
 
-def test():
+    print(ans)
+    print(front)
 
-    a = SortedList()
-    a.add([16, "sushi"])
-    a.add([16, "ramen"])
-    a.add([15, "gg"])
-    print(a)
-
-def NumberOf1(n):
-    return bin(n & 0xffffffff).count('1')
+    if ans > 10 ** 9:
+        return -1
+    else:
+        return ans
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print(NumberOf1(5))
+    # d = {
+    #     2: [0, 2, 3],
+    #     4: [1],
+    #     5:[0, 1, 4],
+    #     6:[2]
+    # }
+    #
+    # a = sorted(d.items(), key=lambda x: len(x[1]), reverse=False)
+    # print(a)
+    S = 'RW' * 100000
+    print(solution(S))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
